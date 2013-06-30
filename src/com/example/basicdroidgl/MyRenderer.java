a02faf62e06b02eb026528653b4ace12df83bbb8
Mikey <pmsorhaindo@gmail.com>
2013-06-30 22:58:54 +0100

    Camera and transformation and view port transformation now taking place :D

--- src/com/example/basicdroidgl/MyRenderer.java
+++ src/com/example/basicdroidgl/MyRenderer.java
@@ -4,12 +4,18 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
 import com.example.basicdroidgl.shapedata.Triangle;
 
 
 public class MyRenderer implements GLSurfaceView.Renderer {
 
     private Triangle mTriangle;
+    
+    float[] mProjMatrix = new float[16]; //must assign space for these variable otherwise NullPointerException errors will come.
+	float[] mMVPMatrix = new float[16];
+	float[] mVMatrix = new float[16];
 
     @Override
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
@@ -22,12 +28,19 @@ public class MyRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onDrawFrame(GL10 unused) {
+    	
+    	// Set the camera position (View Matrix)
+    	Matrix.setLookAtM(mVMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+    	
+    	//Calculate the projection and view transformation
+    	
+    	Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0, mVMatrix, 0);
 
         // Draw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
         // Draw triangle
-        mTriangle.draw();
+        mTriangle.draw(mMVPMatrix);
     }
 
     @Override
@@ -35,6 +48,12 @@ public class MyRenderer implements GLSurfaceView.Renderer {
         // Adjust the viewport based on geometry changes,
         // such as screen rotation
         GLES20.glViewport(0, 0, width, height);
+        
+        float ratio = (float)width/height;
+        
+		// This matrix is applied to the object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(mProjMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 
     public static int loadShader(int type, String shaderCode){
--- src/com/example/basicdroidgl/shapedata/Triangle.java
+++ src/com/example/basicdroidgl/shapedata/Triangle.java
@@ -17,10 +17,15 @@ public class Triangle {
     private int mColorHandle;
 	
     private final String vertexShaderCode =
-        "attribute vec4 vPosition;" +
-        "void main() {" +
-        "  gl_Position = vPosition;" +
-        "}";
+            // This matrix member variable provides a hook to manipulate
+            // the coordinates of the objects that use this vertex shader
+            "uniform mat4 uMVPMatrix;" +
+
+            "attribute vec4 vPosition;" +
+            "void main() {" +
+            // the matrix must be included as a modifier of gl_Position
+            "  gl_Position = vPosition * uMVPMatrix;" +
+            "}";
 
     private final String fragmentShaderCode =
         "precision mediump float;" +
@@ -42,7 +47,9 @@ public class Triangle {
 
     // Set color with red, green, blue and alpha (opacity) values
     float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
-
+    
+    private int mMVPMatrixHandle;
+    
     /**
      * Triangle Shape Renderer's Constructor
      */
@@ -74,7 +81,7 @@ public class Triangle {
 
     }
 
-    public void draw() {
+    public void draw(float[] mvpMatrix) {
         // Add program to OpenGL environment
         GLES20.glUseProgram(mProgram);
 
@@ -94,7 +101,13 @@ public class Triangle {
 
         // Set color for drawing the triangle
         GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+        
+        // get handle to shape's transformation matrix
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
 
+        // Apply the projection and view transformation
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
+        
         // Draw the triangle
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
 
